a5845c45507654707b977b56153e1c0ea1810615
Roger Forner Fabre <hello@rogerforner.com>
2017-01-31 16:16:39 +0100

    Weather.vue

---
+++ src/components/Weather.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="weather">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weather',
+  data () {
+    return {
+      msg: 'Here weather todo'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
